109383f9f4cfb83b67a9bba86665a4d55f302e9c
HARIMUHAMMADRAMDAN <121023797+HARIMUHAMMADRAMDAN@users.noreply.github.com>
2024-06-27 14:42:14 +0700

    Delete UAS-PBO-BOOKING JADWAL FUTSAL-HARI MUHAMMAD RAMDAN/futsal_booking-second/futsalbooking directory

--- UAS-PBO-BOOKING JADWAL FUTSAL-HARI MUHAMMAD RAMDAN/futsal_booking-second/futsalbooking/src/main/java/futsal/com/futsalbooking/repositories/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package futsal.com.futsalbooking.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import futsal.com.futsalbooking.models.Item;
-
-public interface ItemRepository extends JpaRepository<Item,Integer> {
-    
-}
